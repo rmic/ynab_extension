98e96b101501d302580ebb4864b5198f33b9873a
Raphaël Michel <raph.mic@gmail.com>
2020-05-15 22:20:18 +0200

    Hides the left side menu as well, button floats on the right side button is now made of icons

--- contentScript.js
+++ contentScript.js
@@ -4,28 +4,41 @@ function addButton() {
     if (toolbar) {
         var newBtn = document.createElement("button");
         newBtn.innerText = "Hide";
-        newBtn.id = "hideRightSideBtn";
+        newBtn.id = "hideSidesBtn";
         newBtn.className = "button";
+        newBtn.style ="float: right;";
 
-        function hideRightSide() {
+        function hideSides() {
             var budgetInspector = document.getElementsByClassName("budget-inspector")[0];
             budgetInspector.style = "display:none";
+            var sidebar = document.getElementsByClassName("sidebar")[0];
+            sidebar.style = "display:none";
+            var content = document.getElementsByClassName("content")[0];
+            content.style = "width:100%; left: 0px;";
+            var budgetHeader = document.getElementsByClassName("budget-header")[0];
+            budgetHeader.style = "width:100%; left: 0px;";
             var budgetTable = document.getElementsByClassName("budget-table-container")[0];
             budgetTable.style = "width:100%";
-            newBtn.onclick = showRightSide;
-            newBtn.innerText = "Show";
+            newBtn.onclick = showSides;
+            newBtn.innerText = "⇥ ⇤";
         }
 
-        function showRightSide() {
+        function showSides() {
             var budgetInspector = document.getElementsByClassName("budget-inspector")[0];
             budgetInspector.style = "";
+            var sidebar = document.getElementsByClassName("sidebar")[0];
+            sidebar.style = "";
+            var budgetHeader = document.getElementsByClassName("budget-header")[0];
+            budgetHeader.style = "";
+            var content = document.getElementsByClassName("content")[0];
+            content.style = "";
             var budgetTable = document.getElementsByClassName("budget-table-container")[0];
             budgetTable.style = "";
-            newBtn.onclick = hideRightSide;
-            newBtn.innerText = "Hide";
+            newBtn.onclick = hideSides;
+            newBtn.innerText = "⇤ ⇥";
         }
 
-        newBtn.onclick = hideRightSide;
+        newBtn.onclick = hideSides;
         toolbar.appendChild(newBtn);
         return true;
     }
